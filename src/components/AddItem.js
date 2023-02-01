e98757a1dfbce5ff4b07b3396df58226af56fb0d
Dos(신동오) <doscode.kr@gmail.com>
2023-02-01 23:28:34 +0900

    # leave comments on AddItem

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/*
+클릭이 가능하다고 표시하고 싶은건 알겠어요.
+그럼 span tag 또는 div 태그에서 mouse cursor를 변경하면 될거 같아요.
+용도에 맞는 코드를 정확히 사용하는게 좋아요. 이런 코드는 문제가 있어요.
+*/
 const AddItem = () => {
     return (
         <a
