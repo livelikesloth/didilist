d4320c5bff4542bc429aa9d4cdf0755ac9fa74a9
Dos(신동오) <doscode.kr@gmail.com>
2023-02-01 23:23:08 +0900

    # apply lint on AddItem.js

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React from 'react';
 
-const AddItem = () =>{
-
-  return(
-    <a href="/" className="button_" onClick={event=>{
-      event.preventDefault();
-    }}>+</a>
-  )
-}
+const AddItem = () => {
+    return (
+        <a
+            href="/"
+            className="button_"
+            onClick={event => {
+                event.preventDefault();
+            }}
+        >
+            +
+        </a>
+    );
+};
 
 export default AddItem;
